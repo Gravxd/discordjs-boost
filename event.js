f1204ddc94d6de7797cc4d6e191b20b5da0c1ae5
Grav <75702884+Gravxd@users.noreply.github.com>
2021-06-27 00:58:24 +0100

    Update event.js

--- event.js
+++ event.js
@@ -1,13 +1,15 @@
 client.on('guildMemberUpdate', (oldMember, newMember) => {
     if(oldMember.user.bot || newMember.user.bot) return;
-    const booster_role = newMember.guild.roles.find(r => r.id === 'boosteroleidhere')
-    const welcome_channel = newMember.guild.channels.find(c => c.id === 'welcomechannelid')
+    const booster_role = newMember.guild.roles.get('booster_role_id)') || newMember.guild.roles.find(r => r.name.toLowerCase() === 'booster_role_name')
+    const welcome_channel = newMember.guild.channels.get('welcome_channel_id') || newMember.guild.channels.find(c => c.name.toLowerCaser() === 'welcome_channel_name');
     const boost_embed = new RichEmbed()
     .setColor("#4632a8")
     .setTitle(`**New Booster!**`)
     .setDescription(`Thank you to ${newMember} for the boost!`)
     .setThumbnail(newMember.user.displayAvatarURL)
-    if(!oldMember.roles.has(booster_role.id) && newMember.roles.has(booster_role.id)) {
+   if(booster_role) {
+     if(!oldMember.roles.has(booster_role.id) && newMember.roles.has(booster_role.id) && welcome_channel) {
         welcome_channel.send(boost_embed)
     }
+   }
 })
